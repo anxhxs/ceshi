013d26de04d7749ea02118dc1ce0228b2e0588fb
anxhxs <anxionghao@bogon>
2016-04-12 17:49:00 +0800

    Initial Commit

---
+++ ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshi
+//
+//  Created by 安雄浩 on 16/4/12.
+//  Copyright © 2016年 安雄浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ceshi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ceshi
+//
+//  Created by 安雄浩 on 16/4/12.
+//  Copyright © 2016年 安雄浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
